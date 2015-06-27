8bbadcb396f720b9b217f9f674f01dd92a1c7e0d
jhl52771 <610262374@qq.com>
2015-06-27 15:22:27 +0800

    add jqgrid comm tool

---
+++ NFD/Content/Scripts/View/girdHelper.js
@@ -0,0 +1,20 @@
+﻿function JQgridSaved(formId, rollback) {
+    setInterval(function () {
+        var fdoForm = $("#" + formId);
+        if (fdoForm.size() > 0) {
+            rollback(fdoForm);
+        }
+    }, 1000)
+}
+
+
+function JQgridSaveBindByFirst(gridId, formObj, id) {
+    var data = $("#"+gridId).jqGrid("getRowData");
+    if ($(data).size() > 1) {
+        var rowFirst = data[0];
+        var jno = formObj.find("#"+id);
+        if (jno.val() == "") {
+            jno.val(rowFirst[id]);
+        }
+    }
+}
