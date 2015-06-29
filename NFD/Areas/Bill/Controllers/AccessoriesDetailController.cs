51c828abaff4b8ce9e0eaa64717287ab397155ff
sunkaixuan <610262374@qq.com>
2015-06-29 22:23:31 +0800

    gg

--- NFD/Areas/Bill/Controllers/AccessoriesDetailController.cs
+++ NFD/Areas/Bill/Controllers/AccessoriesDetailController.cs
@@ -7,6 +7,9 @@ using Trirand.Web.Mvc;
 using NFD.BLL;
 using NFD.Entities.Data;
 using NFD.BLL.Bill;
+using System.Data;
+using COM.Utility;
+using COM.Extension;
 namespace NFD.Areas.Bill.Controllers
 {
     /// <summary>
@@ -69,12 +72,18 @@ namespace NFD.Areas.Bill.Controllers
                 {
                     ShowSearchToolBar = true,
                     CustomButtons = new List<JQGridToolBarButton>() { 
+                         new JQGridToolBarButton(){
+                       Text="导出EXCEL",
+                        OnClick="exExcel"
+                      } ,
                     new JQGridToolBarButton(){
                        OnClick="edit",
                        Text="编辑该定单",
                         ButtonIcon="ui-icon-pencil"
                       }
+                      
                     }
+                  
                 };
                 reval.AddDialogSettings = new AddDialogSettings()
                 {
@@ -283,5 +292,70 @@ namespace NFD.Areas.Bill.Controllers
 
         }
 
+
+        /// <summary>
+        /// 导出excel
+        /// </summary>
+        /// <param name="trader_id"></param>
+        [OutputCache(Duration = 0)]
+        public void ExportExcel(DateTime? dateMin, DateTime? dateMax, int trader_id = 0, int typeId = 0)/* 0、excel 1、pdf*/
+        {
+            using (NFDEntities db = new NFDEntities())
+            {
+                DataTable dt = GetExportDataTable(dateMin, dateMax, trader_id, db);
+                AsposeExcel.MergeCellExport(dt, "新丝路国际贸易公司辅料单.xls");
+
+            }
+        }
+
+        private static DataTable GetExportDataTable(DateTime? dateMin, DateTime? dateMax, int trader_id, NFDEntities db)
+        {
+            var dbList = AccessoriesDetailManager.GetV_AccessoriesDetail(db);
+            if (trader_id > 0)
+            {
+                dbList = dbList.Where(c => c.trader_id == trader_id);
+            }
+            if (dateMin != null)
+            {
+                dbList = dbList.Where(c => c.create_time >= dateMin);
+            }
+            if (dateMax != null)
+            {
+                dateMax = ((DateTime)dateMax).AddDays(1);
+                dbList = dbList.Where(c => c.create_time <= dateMax);
+            }
+            var data = dbList.OrderByDescending(c => c.ad_id).ToList();
+            var traderList = BLL.TraderManager.GetList();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("客户", typeof(string));
+            dt.Columns.Add("辅料名称", typeof(string));
+            dt.Columns.Add("规格", typeof(string));
+            dt.Columns.Add("单价", typeof(string));
+            dt.Columns.Add("单位", typeof(string));
+            dt.Columns.Add("订单数", typeof(string));
+            dt.Columns.Add("到货数量", typeof(string));
+            dt.Columns.Add("使用数量", typeof(string));
+            dt.Columns.Add("辅料总价", typeof(string));
+            dt.Columns.Add("交货时间", typeof(string));
+            dt.Columns.Add("供应商", typeof(string));
+            dt.Columns.Add("创建人", typeof(string));
+            foreach (var r in data)
+            {
+                DataRow dr = dt.NewRow();
+                dr["客户"] = traderList.Single(c => c.trader_id == r.trader_id).name;
+                dr["辅料名称"]=r.ad_name;
+                dr["规格"] = r.specifications;
+                dr["单价"] = r.price.ToMoney();
+                dr["单位"] = r.unit;
+                dr["订单数"] = r.order_num;
+                dr["到货数量"] = r.get_num;
+                dr["使用数量"] = r.use_num;
+                dr["辅料总价"] = r.tol_price.ToMoney();
+                dr["供应商"] = r.supplier_name;
+                dr["创建人"]=r.creator_name;
+                dt.Rows.Add(dr);
+            }
+            return dt;
+        }
     }
 }
